56d5f76fd24a82e02afe3da89f3a1febe30e683c
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-04 09:23:58 +0200

    Gjort Uppgift A

--- src/App.java
+++ src/App.java
@@ -1,6 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Uppgift A");
+        int tal1=23;
+        int tal2=45;
+        int summa;
+        int produkt;
+        summa=tal1+tal2;
+        System.out.println(tal1+"+"+tal2+"="+summa);
+
+        summa=tal1-tal2;
+        System.out.println(tal1+"-"+tal2+"="+summa);
+
+        
         
     }
 }
